b26f8956eb856e7c7777e43a1fd68d17a9c969e3
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2023-12-04 11:58:15 -0500

    Finished Day1

--- Day 1/Day1.R
+++ Day 1/Day1.R
@@ -3,7 +3,7 @@ part <- 2 #Specify 1 or 2
 words <- c("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
 nums <- c("o1e", "t2o", "t3e", "f4r", "f5e", "s6x", "s7n", "e8t", "n9e")
 
-if(part == 2) {
+if (part == 2) {
   for (i in 1:9){
     input <- gsub(words[i], nums[i], input)
   }
