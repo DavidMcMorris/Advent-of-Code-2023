5a229fea63753299f1b99a1c041f5a9f79a9e06b
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2024-01-28 15:34:16 -0500

    Finished part 2

--- Day 4/Day4.R
+++ Day 4/Day4.R
@@ -1,12 +1,29 @@
 input <- readLines("input.txt")
-
 input <- strsplit(input, ": ")
 input <- sapply(input, "[", 2)
 input <- strsplit(input, "\\|")
 
+# Store lists of winning numbers and numbers on tickets
 winners <- lapply(input, function(x) {read.table(text = x[[1]])})
 numbers <- lapply(input, function(x) {read.table(text = x[[2]])})
 
-num_wins <- t(mapply(function(x, y) {(x %in% y)}, x = winners, y = numbers))
-score <- sum(floor(2^(rowSums(num_wins) - 1)))
+# Find number of winning numbers on each ticket
+num_wins_mat <- t(mapply(function(x, y) {(x %in% y)}, x = winners, y = numbers))
+num_wins <- rowSums(num_wins_mat)
+
+# Count number of each ticket based on part 2 rules
+num_tickets <- rep(1, length(num_wins))
+for (i in seq_along(num_tickets)) {
+  if (num_wins[i] > 0) {
+    ind <- i + num_wins[i]
+    num_tickets[(i + 1):ind] <- num_tickets[(i + 1):ind] + num_tickets[i]
+  }
+}
+
+# Part 1 Answer
+score <- sum(floor(2^(num_wins - 1)))
 print(score)
+
+# Part 2 Answer
+num_winning_tickets <- sum(num_tickets)
+print(num_winning_tickets)
