070e42900da6dcfe8d4834890a04d4580dae70b3
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2024-06-10 17:30:46 -0400

    Finished part 1

--- Day 8/Day8.R
+++ Day 8/Day8.R
@@ -1,6 +1,24 @@
-inst <- readLines("input.txt", 1)
+input <- "input.txt"
+inst <- readLines(input, 1)
 inst <- strsplit(inst, split = "")[[1]]
-inst[which(inst == "L")] <- 1
-inst[which(inst == "R")] <- 2
+inst[which(inst == "L")] <- 2
+inst[which(inst == "R")] <- 3
+inst <- as.numeric(inst)
 
-nodes <- 
+nodes <- read.table(input, skip = 2, sep = "=")
+
+lr <- unlist(lapply(nodes[, 2],function(x){strsplit(x, split = "\\,| \\(|\\)| ")[[1]][c(2, 4)]}))
+nodes <- cbind(nodes[, 1], matrix(lr, ncol = 2, byrow = TRUE))
+nodes[, 1] <- unlist(lapply(nodes[, 1], function(x){strsplit(x, split = " ")[[1]][1]}))
+
+len <- length(inst)
+i <- 0
+current <- "AAA"
+while (current != "ZZZ") {
+  i <- i + 1
+  j <- (i - 1) %% len + 1
+  row <- which(nodes[, 1] == current)
+  current <- nodes[row, inst[j]]
+}
+
+print(i)
