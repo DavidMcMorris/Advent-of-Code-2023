49c061e02d55cdb2dbdc66a671c80f994a791d2b
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2024-06-25 16:01:19 -0400

    Finished both parts

--- Day 9/Day9.R
+++ Day 9/Day9.R
@@ -12,9 +12,24 @@ next_value <- function(hist_seq) {
   }
 }
 
-total <- 0
+total_next <- 0
 for (i in 1:len) {
-  total <- total + next_value(hists[i, ])
+  total_next <- total_next + next_value(hists[i, ])
 }
 
-print(total)
+prev_value <- function(hist_seq) {
+  value <- hist_seq[1]
+  if (identical(as.numeric(unique(hist_seq)), 0)) {
+    return(value)
+  } else {
+    hist_seq <- diff(hist_seq)
+    return(value - prev_value(hist_seq))
+  }
+}
+
+total_prev <- 0
+for (i in 1:len) {
+  total_prev <- total_prev + prev_value(hists[i, ])
+}
+
+print(c(total_next, total_prev))
