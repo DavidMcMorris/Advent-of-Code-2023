be0d7f1e0273f04ca5331e08ff810243b5b2ad14
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2024-07-08 14:50:30 -0400

    Finished part 1

--- Day 11/Day11.R
+++ Day 11/Day11.R
@@ -1,4 +1,27 @@
-input <- "sample.txt"
+input <- "input.txt"
 space <- readLines(input)
 space <- lapply(space, strsplit, split = "")
 space <- matrix(unlist(space), nrow = length(space), byrow = TRUE)
+
+gal_inds <- which(space == "#")
+num_gal <- length(gal_inds)
+space[gal_inds] <- seq_len(num_gal)
+dims <- dim(space)
+
+exp_rows <- which(rowSums(space == ".") == ncol(space))
+exp_cols <- which(colSums(space == ".") == nrow(space))
+
+total_distance <- 0
+for (i in seq_len(num_gal - 1)) {
+  current_gal <- arrayInd(gal_inds[i], dims)
+  for (j in (i + 1):num_gal) {
+    next_gal <- arrayInd(gal_inds[j], dims)
+    extra_rows <- sum(current_gal[1] < exp_rows & exp_rows < next_gal[1]) +
+      sum(current_gal[1] > exp_rows & exp_rows > next_gal[1])
+    extra_cols <- sum(current_gal[2] < exp_cols & exp_cols < next_gal[2]) +
+      sum(current_gal[2] > exp_cols & exp_cols > next_gal[2])
+    distance <- extra_rows + extra_cols + sum(abs(next_gal - current_gal))
+    total_distance <- total_distance + distance
+  }
+}
+print(total_distance)
