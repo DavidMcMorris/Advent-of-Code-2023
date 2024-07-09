8b12595a0bb963a9e5ef257599533f017b3fa9ff
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2024-07-09 11:35:58 -0400

    Can read/organize input

--- Day 13/Day13.R
+++ Day 13/Day13.R
@@ -1,3 +1,13 @@
-input <- "sample.txt"
+input <- "input.txt"
 valley <- readLines(input)
-breaks <- which(valley == "")
+breaks <- c(which(valley == ""), length(valley) + 1)
+valley_ls <- list()
+
+start <- 1
+for (i in seq_len(length(breaks))) {
+  temp <- valley[start: (breaks[i] - 1)]
+  temp <- lapply(temp, strsplit, split = "")
+  temp <- matrix(unlist(temp), nrow = length(temp), byrow = TRUE)
+  valley_ls[[i]] <- temp
+  start <- breaks[i] + 1
+}
