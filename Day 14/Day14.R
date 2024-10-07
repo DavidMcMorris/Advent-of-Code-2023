018c0eb257e7ff005bc61366a92fc642a2d0cece
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2024-10-07 15:15:16 -0400

    Finished part 1

--- Day 14/Day14.R
+++ Day 14/Day14.R
@@ -1,13 +1,29 @@
-input <- "sample.txt"
+input <- "input.txt"
 mirror <-  as.matrix(read.table(input, sep = ""))
 mirror <- strsplit(mirror, "")
-mirror <- matrix(unlist(mirror), ncol = length(x), byrow = TRUE)
-mirror2 <- mirror
+mirror <- matrix(unlist(mirror), ncol = length(mirror), byrow = TRUE)
+mirror_dim <- dim(mirror)
 
 num_rounds <- sum(mirror == "O")
-inds <- which(mirror == "O"
-)
-for (i in 1:num_rounds) {
-  ind <- inds[i]
-  up <- ind - 1
+north <- 1 + mirror_dim[1] * seq(0, mirror_dim[2])
+south <- north + mirror_dim[1] - 1
+
+for (i in seq_len(mirror_dim[2])) {
+  flag <- 0
+  j <- 1
+  while (flag == 0) {
+    if (mirror[j, i] == "." && mirror[j + 1, i] == "O") {
+      mirror[j, i] <- "O"
+      mirror[j + 1, i] <- "."
+      j <- max(j - 1, 1)
+    } else {
+      j <- j + 1
+    }
+    if (j == mirror_dim[1]) {
+      flag <- 1
+    }
+  }
 }
+
+ans <- sum(rowSums(mirror == "O") * rev(seq(1:mirror_dim[1])))
+print(ans)
